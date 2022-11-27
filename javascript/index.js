63b95e52c23e2db007b3c6a620756a89b64b6147
VIKAS KUMAR <kumar.vikas593@gmail.com>
2022-11-27 19:25:01 +0530

    js

--- javascript/index.js
+++ javascript/index.js
@@ -71,7 +71,7 @@ if(a){
 
 
 
-
+/*
 //true
 var a = "abcd";
 if(a){
@@ -79,7 +79,7 @@ if(a){
 }else {
   console.log("false");
 }
-
+*/
 
 
 
@@ -96,6 +96,21 @@ if(a){
 */
 
 
+//------------------------------------------------loops----------------------------------------------------------------------------
+
+/*
+var i =1
+while(i<=5){
+  console.log(i);
+  i++;
+}
+*/
+//ouput
+// 1
+// 2
+// 3
+// 4
+// 5
 
 
 
@@ -104,6 +119,179 @@ if(a){
 
 
 
+/*
+var i =1;
+for(i=1; i<=5; i++){
+  console.log(i);
+}
+*/
+//ouput
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+
+
+
+
+/*
+var i =1;
+do{
+    console.log(i);
+ i++
+}
+while(i<=5);
+  */
+//ouput
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+
+/*
+//undefined
+var a;
+console.log(a);
+*/
+
+
+/*
+//NaN
+var a = "A";
+console.log(65 - a);
+*/
+
+
+
+/*
+var a = 10;
+ console.log(a)
+ a = " 'Coding Ninjas' ";
+console.log(a);
+*/
+//output
+// 10
+// 'Coding Ninjas' 
+// function and arrays.js:11 Uncaught ReferenceError: infinity is not defined
+//     at function and arrays.js:11:2
+// (anonymous) @ function and arrays.js:11
+
+/*
+//11
+console.log(1 + '1')
+
+
+
+//Infinity
+console.log(Infinity+1)
+
+
+//100
+console.log(Math.pow(10,2))
+
+
+//1.7976931348623157e+308
+console.log(Number.MAX_VALUE)
+
+//nan
+console.log(Math.sqrt(-1))
+
+//100
+console.log(parseInt("100"))
+
+
+//NaN
+console.log(parseInt("aaaa"))
+
+
+//number
+console.log(typeof 10)
+
+*/
+
+
+
+
+
+
+//-----------------typeof----------------
+
+/*
+//number
+var a =10;
+console.log(a)
+typeof a
+console.log(typeof a)
+
+
+//boolean
+var a =true;
+console.log(a)
+typeof a
+console.log(typeof a)
+
+
+
+//string
+var a ="aaa";
+console.log(a)
+typeof a
+console.log(typeof a)
+
+
+
+
+
+
+//undefined
+var b;
+console.log(b)
+typeof b
+console.log(typeof b)
+
+
+
+//object
+var a =null;
+console.log(a)
+typeof a
+console.log(typeof a)
+
+
+
+
+//number
+console.log(typeof Infinity)
+
+
+
+
+//number
+console.log(typeof NaN)
+
+*/
+
+
+
+
+//------------operators--------------------
+/*
+//12
+console.log(10+2 )
+
+//6.5
+console.log(13/2)
+
+
+//1.5
+console.log(15.5%2)
+*/
 
 
 
@@ -111,17 +299,81 @@ if(a){
 
 
 
+//-----------------assignment operator--------
+
+//12
+var a = 10;
+console.log(a+=2)
+
+
+//24
+console.log(a*=2)
+
+//-------------urinary opearator
+
+//24
+console.log(a++)
+
+
+
+//25
+console.log(a)
+
+
+
+//26
+console.log(++a)
+
+
+
+//------------comparison opearator
+//false   {isme upar se a = 26 le liya hai}
+var b = 27
+console.log(a ==b)
+
+
+
+//---------------logical opearator-----------
+
+//true
+console.log(a<30&&b>10)
+
+
+
+//--------------type of coercion---------
+
+
+console.log("hello "+'world')
+
+
+
+
+console.log()
+
+
+
+console.log(1==1)
+
+
+
+
+console.log()
+
+
+
+
+console.log()
+
+
+
+
+console.log()
 
 
 
 
 
-
-
-
-
-
-
+console.log()
 
 
 
