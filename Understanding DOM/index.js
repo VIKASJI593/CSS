148dd7602a8570c9ddd44fee12125b55a66aaba0
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-01-07 18:33:24 +0530

    dom

---
+++ Understanding DOM/index.js
@@ -0,0 +1,53 @@
+
+/*
+console.log(document.head)
+//<head> ..  </head>
+
+//console.log(document.body)
+// <body> ..  </body>
+
+console.log(window)
+//Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+
+
+console.log(document)
+// #document
+
+console.log(window.document)
+// #document
+
+console.log(window.screen)
+//Screen {availWidth: 1360, availHeight: 695, width: 1360, height: 768, colorDepth: 24, …}
+
+
+console.log(screen)
+//Screen {availWidth: 1360, availHeight: 695, width: 1360, height: 768, colorDepth: 24, …}
+*/
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------------------------------Fetching Elements-----------------------------------*/
+
+
+//document.getElementById('one')
+console.log(document.getElementById('one'))
+// #document
+
+
+
+
+
+
+
+
+
+
+
+
